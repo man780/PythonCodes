36ff3e9bcc91036dc57c25c1fe2b204b6bc8fb3a
Murod Smith <man780@mail.ru>
2020-08-09 01:44:49 +0500

    write data to csv added

--- run_csv.py
+++ run_csv.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 import csv
 
-with open('lis.csv', newline='') as f:
-    reader = csv.reader(f)
+with open('lis.csv', newline='') as fileImport:
+    reader = csv.reader(fileImport)
     data = list(reader)
+
+bCandidat = []
+bCandidat.append(data[0])
+for candidat in data:
+    if candidat[5] == 'Бошқарма бошлиғи':
+        bCandidat.append(candidat)
+
+
+with open('export/candidates.csv', 'w') as fileExport:
+    writer = csv.writer(fileExport)
+    writer.writerows(bCandidat)
