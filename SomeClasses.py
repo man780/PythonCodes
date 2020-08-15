eae955c41140fb687be36ed7160ffb7dadaa3e4d
Murod Smith <man780@mail.ru>
2020-08-16 00:41:12 +0500

    Fibonacci iterator done

---
+++ SomeClasses.py
@@ -0,0 +1,17 @@
+class Fibonacci(object):
+    def __init__(self, count):
+        self.count = count
+
+    def __iter__(self):
+        a, b = 0, 1
+        for x in range(self.count):
+            if x < 2:
+                yield x
+            else:
+                c = a + b
+                yield c
+                a, b = b, c
+
+
+for x in Fibonacci(15):
+    print(x)
